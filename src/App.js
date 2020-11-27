6aad4b0f03cf1f46e7f1f23d44162dce6f0da355
matheus <matheusg@itmchannelmarketing.com>
2020-11-27 14:47:03 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,131 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const BemVindo = () => {
+    return (
+        <h2> Bem-Vindo(a) </h2>
+    );
 }
 
-export default App;
+const BemVindoPros = (props) => {
+    return (
+        <h2> Bem-Vindo(a) {props.nome} </h2>
+    );
+}
+
+const Equipe = (props) => {
+    return (
+        <div>
+            <Sobre userName={props.nome} cargo={props.cargo} idade={props.idade}/>
+        </div>
+    );
+}
+
+const Sobre = (props) => {
+    return (
+        <div>
+            <h2> Olá sou o(a)  {props.userName} </h2>
+        </div>
+    )
+}
+
+
+class EquipeTeste extends Component{
+    render(){
+        return(
+            <div>
+                <h3> {this.props.nome} </h3>
+            </div>
+        );
+    }
+}
+
+
+
+
+
+function Exemplo(){
+    return (
+        <div>
+            <h1> Olá Mundo </h1>
+            <BemVindo/>
+            
+            <BemVindoPros nome="matheus"/>
+
+            <h1>Conheça a nossa equipe: </h1>
+            <Equipe nome="matheus" cargo="dev" idade="24"/>
+
+
+            <EquipeTeste nome="matheus"/>
+
+        </div>
+
+        
+            
+
+    ); 
+}
+
+class App extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            nome: "Matheus",
+            contador: 0,
+            hora: "00:00:00"
+        }
+        this.aumenter = this.aumenter.bind(this);
+        this.diminuir = this.diminuir.bind(this);
+    }
+
+    componentDidMount(){
+        setInterval(()=>{
+            this.setState({hora: new Date().toLocaleTimeString()})
+        },1000);
+    }
+
+    componentDidUpdate(){
+        console.log("Autalizou");
+    }
+
+    shouldComponentUpdate(){
+        //retorna true or false
+    }
+
+    aumenter(){
+        let state = this.state;
+        state.contador += 1;
+        this.setState(state)
+    }
+
+    diminuir(){
+        let state = this.state;
+        if(state.contador === 0){
+            alert("chegou a zerp");
+            return;
+        }
+
+        state.contador -= 1;
+        this.setState(state)
+    }
+
+    render(){
+        return(
+            <div>
+                <h2>{this.state.nome}</h2>
+                <h3>Contador</h3>
+                <h3>
+                <button onClick={this.diminuir}> - </button>
+                 {this.state.contador} 
+                <button onClick={this.aumenter}> + </button>
+                </h3>
+                <h3>
+                    Horario {this.state.hora}
+                </h3>
+
+            </div>
+        );
+    }
+}
+
+export default App
